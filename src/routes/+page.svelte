f552adc99ea55dd8536812ba6180a0d918e7d7f9
fizipan <hafizhmy26@gmail.com>
2025-05-17 10:38:09 +0700

    chore: initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3xl font-bold text-red-500 underline">Welcome to SvelteKit</h1>
+<p class="text-lg">
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
